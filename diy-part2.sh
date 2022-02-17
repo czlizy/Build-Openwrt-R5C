e30b930eb5ab88ae05e3fcdda4a067cfbc33b9fc
ZZMHA227 <57690855+ZZMHA227@users.noreply.github.com>
2022-02-17 20:30:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,21 +13,6 @@
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' luci/collections/luci/Makefile
 
-# Set to local feeds
-pushd customfeeds/packages
-export packages_feed="$(pwd)"
-popd
-pushd customfeeds/luci
-export luci_feed="$(pwd)"
-popd
-sed -i '/src-git packages/d' feeds.conf.default
-echo "src-link packages $packages_feed" >> feeds.conf.default
-sed -i '/src-git luci/d' feeds.conf.default
-echo "src-link luci $luci_feed" >> feeds.conf.default
-
-# Update feeds
-./scripts/feeds update -a
-
 # Add openclash
 git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 
@@ -47,5 +32,5 @@ git clone --depth=1 https://github.com/tty228/luci-app-serverchan
 git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
 
 # luci-app-cpufreq
-sed -i "s/@arm/@(arm||aarch64)/g" luci/applications/luci-app-cpufreq/Makefile
-sed -i "s/"services"/"system"/g" luci/applications/luci-app-cpufreq/luasrc/controller/cpufreq.lua
+#sed -i "s/@arm/@(arm||aarch64)/g" luci/applications/luci-app-cpufreq/Makefile
+#sed -i "s/"services"/"system"/g" luci/applications/luci-app-cpufreq/luasrc/controller/cpufreq.lua
