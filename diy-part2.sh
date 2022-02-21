0fcb70258f29c48e833ef846e6dc357f39c7f9e9
ZZMHA227 <57690855+ZZMHA227@users.noreply.github.com>
2022-02-21 09:34:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -42,6 +42,7 @@ git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr
 git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
 
 # Add luci-theme-argon
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+rm -rf ./feeds/luci/themes/luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon 
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
-rm -rf ../../feeds/luci/themes/luci-theme-argon
+
