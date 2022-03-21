1e648cacd4cfd784a33df0894f48dd8a391d337c
ZZMHA227 <57690855+ZZMHA227@users.noreply.github.com>
2022-03-21 20:56:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -31,7 +31,7 @@ mkdir package/community
 pushd package/community
 
 # Add openclash
-git clone --depth=1 -b master https://github.com/vernesong/OpenClash
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash package/luci-app-openclash
 
 # Add luci-app-vssr <M>
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
@@ -44,13 +44,10 @@ git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr
 git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
 
 # Add luci-theme-argon
-rm -rf ./feeds/luci/themes/luci-theme-argon
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 # Add luci-app-dockerman
-rm -rf ./feeds/luci/collections/luci-lib-docker
-rm -rf ./feeds/luci/applications/luci-app-docker
-rm -rf ./feeds/luci/applications/luci-app-dockerman
-git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
-git clone --depth=1 https://github.com/lisaac/luci-lib-docker
+rm -rf feeds/luci/applications/luci-app-dockerman
+git clone --depth=1 https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
