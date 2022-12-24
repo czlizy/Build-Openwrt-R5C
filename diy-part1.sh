01264927958d2f63645aeac05ff6fbdf717ea85e
ZZMHA227 <57690855+ZZMHA227@users.noreply.github.com>
2022-12-25 00:09:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,4 +21,3 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git" >> "feeds.conf.default"
 echo "src-git passwallluci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> "feeds.conf.default"
 #echo "src-git bypass https://github.com/kiddin9/openwrt-bypass.git" >> "feeds.conf.default"
-echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
