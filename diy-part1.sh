9a8964b87e54be87fc818f16c13de42b5791bf87
ZZMHA227 <57690855+ZZMHA227@users.noreply.github.com>
2023-01-02 21:22:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,5 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git" >> "feeds.conf.default"
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> "feeds.conf.default"
 echo "src-git passwallluci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> "feeds.conf.default"
-#echo "src-git bypass https://github.com/kiddin9/openwrt-bypass.git" >> "feeds.conf.default"
