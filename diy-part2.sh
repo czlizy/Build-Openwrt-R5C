ee90f3154ff254088ecb4c86199a9d6aeb69a84e
czlizy <110054901+czlizy@users.noreply.github.com>
2023-01-09 12:33:58 +0800

    Update diy-part2.sh
    use lean's mt76 package

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,7 +21,9 @@ sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" package/lean/
 
 rm -rf package/kernel/mac80211
 rm -rf package/kernel/rtl8821cu
+rm -rf package/kernel/mt76
 svn export https://github.com/coolsnowwolf/lede/trunk/package/kernel/mac80211 package/kernel/mac80211
+svn export https://github.com/coolsnowwolf/lede/trunk/package/kernel/mt76 package/kernel/mt76
 
 
 # luci-app-cpufreq
